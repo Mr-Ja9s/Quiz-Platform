0181cbd2705cec6a1622071e867394a1a70fb1fe
Mr-Ja9s <surya2006jaya@gmail.com>
2026-05-22 06:52:31 +0530

    feat:simple quiz platform using js

---
+++ index.js
@@ -0,0 +1,28 @@
+var pos=0, board, status, qus, choice, A, B, C, core=0;
+var questions=[
+    ["shortcut of copy","ctrl+c","ctrl+v","ctrl+x"],
+    ["10+10","20","8","55","A"],
+    ["10+10","200","800","955","C"],
+    ["10+100","290","8","585","B"]];
+function DisplayQuestion(){
+    board=document.getElementById("board");
+    if(pos>=questions.length){
+        board.innerHTML="<h2>you Got "+core+" out of "+questions.length+"</h2><button onclick='location.reload()'>play again</button>";
+        document.getElementById("status").innerHTML="Quizz completed";
+    }
+    document.getElementById("status").innerHTML="Question "+(pos+1)+" of "+questions.length;
+    qus=questions[pos][0];
+    A=questions[pos][1];
+    B=questions[pos][2];
+    C=questions[pos][3];
+    board.innerHTML="<h3>"+qus+"</h3>";
+    //<button onclick='checkAnswer(\"A\")'>"+A+"</button><button onclick='checkAnswer(\"B\")'>"+B+"</button><button onclick='checkAnswer(\"C\")'>"+C+"</button>";
+    board.innerHTML+="<label><input type='radio' name='choices'  value='"+A+"'>"+A+"</label><br>";
+    board.innerHTML+="<label><input type='radio' name='choices'  value='"+B+"'>"+B+"</label><br>";
+    board.innerHTML+="<label><input type='radio' name='choices'  value='"+C+"'>"+C+"</label><br><br>";
+    board.innerHTML+="<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+function checkAnswer(){
+    choices=document.getElementsByName("choices");
+    for(var i=0;i<choices.length;i++){
+    pos++;
